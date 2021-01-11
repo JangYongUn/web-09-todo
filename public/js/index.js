e03e48838c72b6add8530bd6e1195e235cf17c96
JangYongUn <sm970413@naver.com>
2021-01-11 17:04:21 +0900

    css정리

--- public/js/index.js
+++ public/js/index.js
@@ -6,26 +6,41 @@ var ref = null;
 var google = new firebase.auth.GoogleAuthProvider();
 var facebook = new firebase.auth.FacebookAuthProvider();
 
-/************** 이벤트등록 ***************/
-auth.languageCode = 'ko';
-auth.onAuthStateChanged(onAuthChg);
 
-$('#btGoogleLogin').click(onGoogleLogin);
-$('#btLogout').click(onLogout);
+/************** 사용자함수 ***************/
+function dbInit() {
+	db.ref('root/todo/'+user.uid).on('child_added', onAdd);
+	db.ref('root/todo/'+user.uid).on('child_removed', onRev);
+	db.ref('root/todo/'+user.uid).on('child_changed', onChg);
+}
+
 
 
 /************** 이벤트콜백 ***************/
+function onAdd(r) {
+	console.log(r.val());
+}
+
+function onRev(r) {
+	console.log(r.val());
+}
+
+function onChg(r) {
+	console.log(r.val());
+}
+
+
 function onAuthChg(r) {
-	if(r) {
-		user = r;
+  if(r) {  // 로그인 되었다면..
+    user = r;
 		$('.sign-wrap .icon img').attr('src', user.photoURL);
 		$('.sign-wrap .email').html(user.email);
 		$('.modal-wrapper.auth-wrapper').hide();
     $('#btLogout').show();
     dbInit();
 	}
-	else {
-		user = null;
+	else {  // 로그아웃 되었다면...
+    user = null;
 		$('.sign-wrap .icon img').attr('src', 'https://via.placeholder.com/36');
 		$('.sign-wrap .email').html('');
 		$('.modal-wrapper.auth-wrapper').show();
@@ -34,28 +49,17 @@ function onAuthChg(r) {
 }
 
 function onGoogleLogin() {
-	auth.signInWithPopup(google);
+  auth.signInWithPopup(google);
 }
 
 function onLogout() {
-	auth.signOut();
+  auth.signOut();
 }
 
-function onAdded(r) {
-  $('.list-wrapper').prepend('<div style="padding: 1em; border-bottom:1px solid #ccc">'+r.val().comment+'</div>');
-}
 
-function onsubmit(f) {
-  var comment =f.comment.value;
-  ref.push({
-    comment: comment
-  });
+/************** 이벤트등록 ***************/
+auth.languageCode = 'ko';
+auth.onAuthStateChanged(onAuthChg);  // Watcher: 로그인상태 감시자
 
-  return false;
-}
-
-/************** 사용자함수 ***************/
-function dbInit() {
-  ref = db.ref('root/todo/'+user.uid);
-  ref.on('child_added', onAdded);
-}
+$('#btGoogleLogin').click(onGoogleLogin);
+$('#btLogout').click(onLogout);
