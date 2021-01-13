91d26c986212d7908c54eceb1af883846459e71e
JangYongUn <sm970413@naver.com>
2021-01-13 18:21:31 +0900

    .......

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ var auth = firebase.auth();
 var db = firebase.database();
 var user = null;
 var ref = null;
+var key = null;
 var google = new firebase.auth.GoogleAuthProvider();
 var facebook = new firebase.auth.FacebookAuthProvider();
 
@@ -14,9 +15,133 @@ function dbInit() {
 	db.ref('root/todo/'+user.uid).on('child_changed', onChg);
 }
 
+function addHTML(k, v) {
+	var html = '<li id="'+k+'" class="'+(v.checked ? 'opacity': '')+'">';
+	if(v.checked) {
+		html += '<i class="far fa-circle" onclick="onCheck(\''+k+'\', true);"></i>';
+		html += '<i class="active far fa-check-circle" onclick="onCheck(\''+k+'\', false);"></i>';
+	}
+	else{
+		html += '<i class="active far fa-circle" onclick="onCheck(\''+k+'\', true);"></i>';
+		html += '<i class="far fa-check-circle" onclick="onCheck(\''+k+'\', false);"></i>';
+	}
+	html += '<input type="text" class="ml-3" value="'+v.task+'" onchange="onChange(\''+k+'\', this);" onfocus="onFocus(this);" onblur="onBlur(this);">';
+	html += '<div class="date">'+moment(v.createdAt).format('llll')+'</div>';
+	html += '<button class="btn btn-sm btn-danger bt-delete" onclick="onDelete(\''+k+'\');">삭제</button>';
+	html += '</li>';
+
+	var $li = $(html).prependTo($(".list-wrap"));
+	$li.css("opacity");
+	$li.css("opacity", 1);
+
+	return $li;
+}
+
+function toggleList() {
+	var ref =sb.ref('root/todo/'+user.uid);
+	if( $('.bt-done').hasClass('active') ) {  // 감추기
+		ref.orderByChild('checked').equalTo(false).once('value').then(onGetData);
+	}
+	else{
+		ref.once('value').then(onGetData);
+	}
+}
 
 
 /************** 이벤트콜백 ***************/
+function onReset(f) {
+	f.key.value = '';
+	$('.edit-wrapper').find('button.btn-primary').removeClass('d-none');
+	$('.edit-wrapper').find('button.btn-success').addClass('d-none');
+}
+
+function onGetTask(r) {
+	$('.edit-wrapper').find('form input[name="key"]').val(r.key);
+	$('.edit-wrapper').find('form input[name="task"]').val(r.val().task);
+	$('.edit-wrapper').find('form textarea[name="comment"]').val(r.val().comment);
+	$('.edit-wrapper').find('button.btn-primary').addClass('d-none');
+	$('.edit-wrapper').find('button.btn-success').removeClass('d-none');
+}
+
+function onEdit(f) {
+	var key = f.key.value;
+	var data = {
+		task: f.task.value,
+		comment: f.comment.value,
+		createdAt: new DataCue().getTime(),
+		checked: false
+	};
+	if(key == "") {
+		db.ref('root/todo/'+user.uid).push(data);
+	}
+	else{
+		db.ref('root/todo/'+user.uid+'/'+key).update(data);
+	}
+	f.key.value = '';
+	f.reset();
+	return false;
+}
+
+function onFocus(el) {
+	$(el).parent().addClass('active');
+	key = $(el).parent().attr('id');
+	db.ref('root/todo/'+user.uid+'/'+key).once('value').then(onGetTask);
+}
+
+function onBlur(el) {
+	$(el).parent().removeClass('active');
+}
+
+function onKeyup(el) {
+	$('.edit-wrapper input[name="task"]').val($(el).val());
+}
+
+function onChange(k, v) {
+	db.ref('root/todo/'+user.uid+'/'+k).update({ task: v.value });
+}
+
+function onDelete(key) {
+	db.ref('root/todo/'+user.uid+'/'+key).remove();
+}
+
+function onCheck(key, chk) {
+	db.ref('root/todo/'+user.uid+'/'+key).update({ checked: chk });
+	/*
+	$(el).siblings('i').addClass('active');
+	$(el).removeClass('active');
+	if(chk) {
+		timeout = setTimeout(function(){ 
+			$(el).parent().css('opacity', 0);
+			setTimeout(function(){
+				var data = { checked: true };
+				$(el).parent().remove();
+				db.ref('root/todo/'+user.uid+'/'+$(el).parent().attr('id')).update(data)
+			}, 750) 
+		}, 3000);
+	}
+	else {
+		clearTimeout(timeout);
+	}
+	*/
+}
+
+function onDoneClick() {
+	$('.bt-done').toggleClass('active');
+	toggleList();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 var timeout;
 function onCheck(el, chk) {
 	$(el).siblings('i').addClass('active');
